5973d6456c03c7088a3e40d786ae67d7902eab3c
Collier23 <ryancollier1994@gmail.com>
2022-09-17 13:46:08 +1000

    updated HTML, added local storage for user inputs on first timeblock, need to add for all. Need to remember to commit more often

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+// save button needs to save users input to local storage and it still show when page is refreshed
+
+// save button
+var saveBtn = document.querySelector(".saveBtn");
+// time block
+var timeBlock = document.querySelector(".description");
+// save to local storage
+saveBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    var timeBlock = document.querySelector(".description").value;
+    localStorage.setItem("timeBlock", timeBlock);
+    console.log(timeBlock);
+});
+
+// get from local storage
+var timeBlock = localStorage.getItem("timeBlock");
+console.log(timeBlock);
+
+// display on page
+document.querySelector(".description").innerHTML = timeBlock;
+
+
+
+
+
+
+
+
+
+// get the current date and time
+var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+console.log(currentDay);
+
+// display the current date and time
+$("#currentDay").text(currentDay);
+
+
+var currentHour = moment().format("H");
+console.log(currentHour);
+
+
